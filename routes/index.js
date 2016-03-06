f5ee6db27679c1c8cef9a2c6f6be481a698a76f0
johnnyKuo <johnnykuo@johnnykuodeMacBook-Pro.local>
2016-03-07 00:04:44 +0800

    change the index.ejs and upload some html\js\css files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+var title = 'TsingKuo';
+var navbarHome = '晴空万里';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TsingKuo' });
+  res.render('index', { title: title, navbarHome: navbarHome });
+  res.render('index', { header1: title});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: title, navbarHome: navbarHome });
 });
 
 module.exports = router;
