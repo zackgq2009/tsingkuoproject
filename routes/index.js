8ce80aa3a8f340d21edcea3a2948852b7d9218a2
johnnyKuo <johnnykuo@johnnykuodeMacBook-Pro.local>
2016-03-08 23:12:59 +0800

    fix the bug about scripts file

--- routes/index.js
+++ routes/index.js
@@ -14,35 +14,26 @@ script files address
 var contactmefile = '<script src="/javascripts/contact_me.js"></script>';
 var jqbootstrapvalidationfile = '<script src="/javascripts/jqBootstrapValidation.js"></script>';
 //var npmfile = '<script src="/javascripts/npm.js"></script>';
-//var nullfile = '<!-- This page do not need this javascript -->';
-var carousel = "<script>$('.carousel').carousel({interval: 5000 //changes the speed}) </script>";
-
-/* Get all page. */
-router.get('*', function(req, res, next) {
-  res.render('navbar', { navbarHome: navbarHome});
-  res.render('head', {title: title});
-  next();
-});
+var nullfile = '<!-- This page do not need this javascript -->';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('scripts', {specialjsfiles: carousel});
-  res.render('index', {title: title});
+  res.render('index', {title: title, navbarHome: navbarHome, specialjsfiles: nullfile});
 });
 
 /* Get the about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about')
+  res.render('about', {title: title, navbarHome: navbarHome, specialjsfiles: nullfile});
 });
 
 /* Get the services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services');
+  res.render('services', {title: title, navbarHome: navbarHome, specialjsfiles: nullfile});
 });
 
 /* Get the contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', {specialjsfiles: contactimefile+jqbootstrapvalidationfile});
+  res.render('contact', {title: title, navbarHome: navbarHome, specialjsfiles: contactmefile+jqbootstrapvalidationfile});
 });
 
 
