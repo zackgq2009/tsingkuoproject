8a551c59605c27bdadd73be9d5228828c9153120
qguo <qguo@maxleap.com>
2016-03-14 16:08:56 +0800

    change the mongotest.js and create content.json files

--- mongotest.js
+++ mongotest.js
@@ -2,43 +2,28 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017/comtsingkuo';
 //MongoClient.connect(url, function(err, db) {
 //    assert.equal(null, err);
 //    console.log("Connected correctly to server.");
 //    db.close();
 //})
 
-//var insertDocument = function(db, callback) {
-//    db.collection('restaurants').insertOne( {
-//        "address" : {
-//            "street":"2 Avenue",
-//            "zipcode":"10075",
-//            "building":"1480",
-//            "coord":[-73.9557413, 40.7720266]
-//        },
-//        "borough":"Manhattan",
-//        "cuisine":"Italian",
-//        "grades":[
-//            {
-//                "date" : new Date("2014-10-01T00:00:00Z"),
-//                "grade" : "A",
-//                "score" : 11
-//            },
-//            {
-//                "date" : new Date("2014-01-16T00:00:00Z"),
-//                "grade" : "B",
-//                "score" : 17
-//            }
-//        ],
-//        "name":"Vella",
-//        "restaurant_id":"41704620"
-//    }, function(err, result) {
-//        assert.equal(err, null);
-//        console.log("Inserted a document into the restaurants collection.");
-//        callback();
-//    });
-//};
+var insertDocument = function(db, callback) {
+    db.collection('wordpad').insertOne( {
+        "page":"index",
+        "module":"page content",
+        "name":"panel",
+        "section":"header",
+        "content":"生活经历",
+        "createDate":new Date(),
+        "updateDate":new Date()
+    }, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a document into the restaurants collection.");
+        callback();
+    });
+};
 //
 //MongoClient.connect(url, function(err, db) {
 //    assert.equal(null, err);
@@ -76,26 +61,49 @@ var url = 'mongodb://localhost:27017/test';
 //    );
 //};
 
-var updateRestaurants = function(db, callback) {
-    db.collection('restaurants').replaceOne({
-        "restaurant_id":"41704620"
-    },{
-        "name" : "Vella 2",
-        "address" : {
-            "coord" : [ -73.9557413, 40.7720266 ],
-            "building" : "1480",
-            "street" : "2 Avenue",
-            "zipcode" : "10075"
-        }
-    }, function(err, results) {
-        console.log(results);
-        callback();
-    });
-};
+//var updateRestaurants = function(db, callback) {
+//    db.collection('restaurants').replaceOne({
+//        "restaurant_id":"41704620"
+//    },{
+//        "name" : "Vella 2",
+//        "address" : {
+//            "coord" : [ -73.9557413, 40.7720266 ],
+//            "building" : "1480",
+//            "street" : "2 Avenue",
+//            "zipcode" : "10075"
+//        }
+//    }, function(err, results) {
+//        console.log(results);
+//        callback();
+//    });
+//};
+
+//var removeRestaurants = function(db, callback) {
+//    db.collection('restaurants').deleteMany({
+//        "borough":"Manhattan"
+//    },function(err, results) {
+//        console.log(results);
+//        callback();
+//    });
+//};
+
+//var aggregateRestaurants = function(db, callback) {
+//    db.collection('restaurants').aggregate(
+//        [
+//            {
+//                $group: {"_id": "$borough", "count": { $sum: 1}}
+//            }
+//        ]
+//    ).toArray(function(err, result) {
+//            assert.equal(err, null);
+//            console.log(result);
+//            callback(result);
+//        });
+//};
 
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
-    updateRestaurants(db, function() {
+    insertDocument(db, function() {
         db.close();
     });
 });
